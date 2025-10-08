8a5014e0c6d96d36b524c89922a04b59ab5ee44f
dmitriykalganov <kalganov_dmitriy@mail.ru>
2025-10-08 15:23:02 +1100

    Исправленная домашняя работа: Программа более структурированная; Добавлен собственный контейнер ip_base и тип данных ip_address.

---
+++ ip_base.h
@@ -0,0 +1,58 @@
+#ifndef __IPBASE
+#define __IPBASE
+
+#include <cstddef>
+#include <string>
+#include <vector>
+#include "SimpleAllocator.h"
+#include "ip_address.h"
+
+template<typename T, class _Ax = SimpleAllocator<T> >
+//template<typename T>
+class ip_base
+{
+public:
+    
+     ip_base()
+     {
+      bl = false;
+//        SimpleAllocator<int> alloc;
+//        int* arr = alloc.allocate(10);
+     };
+
+      ip_base(bool b) {bl = b;}
+
+      void push_back(const T &value)
+      {
+  //       if (!std::is_same_v<_Ax, SimpleAllocator<T>>) 
+        if (bl == true) 
+        {
+          baseal.push_back(value);
+        }
+        else 
+          base.push_back(value);
+      };
+
+     std::vector<int>::iterator begin()
+     {
+      if (bl == false) return base.begin();
+      else return baseal.begin();
+     };
+
+     std::vector<int>::iterator end()
+     {
+      if (bl == false) return base.end();
+      else return baseal.end();
+     };
+     int ReadItem (int i)
+     {
+      if (bl == false) return base[i];
+      else return baseal[i];
+     };
+private:
+  std::vector<T> base;		
+	std::vector<T,_Ax> baseal;	
+  bool bl;	
+};
+
+#endif
